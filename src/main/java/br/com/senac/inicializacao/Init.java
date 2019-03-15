f22c6850a1dbaa7544553a119c5442d85e6042f1
Daniel Quelis <danielquelis@icloud.com>
2019-03-14 21:47:11 -0300

    Commit Insert

---
+++ src/main/java/br/com/senac/inicializacao/Init.java
@@ -0,0 +1,26 @@
+package br.com.senac.inicializacao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import br.com.senac.dominio.Aluno;
+import br.com.senac.repositorio.AlunoRepositorio;
+
+@Component
+public class Init implements ApplicationListener<ContextRefreshedEvent>{
+
+	@Autowired
+	AlunoRepositorio alunoRepositorio;
+	
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		
+		Aluno aluno = new Aluno();
+		aluno.setNome("Lucas");
+		
+		alunoRepositorio.save(aluno);
+	}
+	
+}
